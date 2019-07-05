e99a6626b8b4f6b5e31e3f4cfa9f1aace28c6768
MrMhmods <52112618+MrMhmods@users.noreply.github.com>
2019-07-05 14:04:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,45 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+
+
+
+
+
+
+
+
+client.on('message', message => {
+
+  if(message.content === prefix + "user"){
+
+    var embed = new Discord.RichEmbed()
+
+    .setTitle(message.author.tag, message.author.avatarURL)
+
+    .addField(`User`, message.author.username)
+
+    .addField(`Discrim`,`#`+ message.author.discriminator)
+
+    .addField(`Name Color Role`, message.member.colorRole)
+
+    .addField(`Game`,message.author.presence.game ||"Idle.")
+
+    .addField(`Status`,message.author.presence.status)
+
+    message.channel.send(embed);
+
   }
+
 });
+
+
+
+
+
+
+
+
+
 
 client.login('NTk1MTg2MzUzMzkwNzQ3NjUz.XR8jUw.N5IAn-3NH2uTxm0M_PbkdK21nmA');
